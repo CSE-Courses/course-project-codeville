c405389ed41d5c3f229279855806eb691b0a809e
Kielbasa21 <kaszapatryk@gmail.com>
2020-10-18 09:38:42 -0400

    Delete contact.js

--- js/contact.js
+++
@@ -1,7 +0,0 @@
-function setHeight() {
-    var elmnt = document.getElementById("myTopnav");
-    var height=elmnt.offsetHeight;
-    height = height;
-    var elmnt = document.getElementById("container");
-    elmnt.style.setProperty('--header-height',height+'px');
-}
