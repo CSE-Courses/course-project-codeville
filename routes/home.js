35c9b4ae7d3a99ed56f8c3fc9511bdbda0b3a229
prakharpbuf <prakhar.paw08@gmail.com>
2020-12-01 13:33:18 -0500

    dabbr

--- routes/home.js
+++ routes/home.js
@@ -50,11 +50,18 @@ router.post('/education',(req, res, next)=>{
         [req.session.email, major, standing],
         function(err,result){
             if(err) return next(err);
-            res.render('pictureupload');
+            res.redirect('AddCourses');
         }
     );
 });
 
+router.get('/AddCourses', loggedin, (req, res, next) => {
+    connection.query("SELECT DISTINCT dabbr FROM courses", function (err, result) {
+        if (err) return next(err);
+        //console.log(result.rows);
+        res.render('AddCourses', { data: result.rows });
+    });
+});
 
 
 module.exports = router;
