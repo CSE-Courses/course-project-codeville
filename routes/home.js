9042c444c968c0568d466fea38d2b03b7a8b23a9
prakharpbuf <prakhar.paw08@gmail.com>
2020-12-06 23:26:07 -0500

    Completed backend for homepage

--- routes/home.js
+++ routes/home.js
@@ -3,6 +3,7 @@ const path = require('path');
 const connection = require('../db');
 const loggedin = require('../middleware/loggedin')
 const session = require('express-session');
+const { release } = require('os');
 
 const router = express.Router();
 
@@ -13,7 +14,25 @@ router.use(function (req, res, next) {
 });
 
 
-router.get('/HomePage', function (req, res, next) {
+
+router.post("/getfriends",loggedin, async function(req,res){
+    classid=req.body.classid
+    title=req.body.title
+    section=req.body.section
+    //console.log(classid, title,section);
+    str = ""
+    let result = await connection.query("select friend_email from friendstest where email = $1 and status = 0 and friend_email in (select email from studentcourses where classid=$2 and title=$3 and section=$4);", [req.session.email, classid, title, section])
+    for(i=0;i<result.rows.length;i++){  
+        let resul = await connection.query("select first_name, last_name from personal_details where email = $1", [result.rows[i].friend_email])
+        str += resul.rows[0].first_name + " " + resul.rows[0].last_name + "~" + result.rows[i].friend_email + "|"
+    }
+    str = str.slice(0, -1)
+    //console.log(friendNames);
+    res.send(str)
+});
+
+router.get('/HomePage', loggedin, function (req, res, next) {
+
     connection.query('select classid, title, section from studentcourses where email=$1', [req.session.email], function (err, result) {
         if (err) {
             throw err;
@@ -21,18 +40,18 @@ router.get('/HomePage', function (req, res, next) {
         var classids = []
         var titles = []
         var sections = []
-
         for(i=0; i<result.rows.length; i++){
             classids[i]=result.rows[i].classid
             titles[i]=result.rows[i].title
             sections[i]=result.rows[i].section
         }
-        while (classids.indexOf("&lt;&lt;&lt; &gt;&gt;&gt;")!=-1){
-            classids[classids.indexOf("&lt;&lt;&lt; &gt;&gt;&gt;")]="<<< >>>"
-        }
-        res.render('HomePage', {classids: classids, titles:titles, sections:sections})
-    });
-})
+            
+    while (classids.indexOf("&lt;&lt;&lt; &gt;&gt;&gt;")!=-1){
+        classids[classids.indexOf("&lt;&lt;&lt; &gt;&gt;&gt;")]="<<< >>>"
+    }
+        res.render('HomePage', { classids: classids, titles: titles, sections: sections})
+});
+});
 
 
 router.get('/home', loggedin, function(req,res,next){
@@ -60,7 +79,7 @@ router.get('/education',loggedin, (req, res, next) => {
 });
 
 
-router.post('/education',(req, res, next)=>{
+router.post('/education',loggedin,(req, res, next)=>{
     const major = req.body.major;
     const standing = req.body.class_standing;
     //LOGIC FOR VERIFYING INFO
