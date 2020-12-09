a1bbbac2d1aa98c9114c595a25a42ab2e92c9660
Kielbasa21 <kaszapatryk@gmail.com>
2020-12-09 18:11:01 -0500

    Moved friendstest to friends

--- routes/friends.js
+++ routes/friends.js
@@ -12,7 +12,7 @@ router.get('/viewfriends',loggedin, async function (req,res,next) {
 
     //CHANGE EMAIL VALUE FOR result, TESTING ONLY
 
-    let result = await connection.query("SELECT friend_email, status FROM friendstest WHERE email = $1 ORDER BY status ASC", [req.session.email]);
+    let result = await connection.query("SELECT friend_email, status FROM friends WHERE email = $1 ORDER BY status ASC", [req.session.email]);
     let listFriends = result.rows;//Should return all friends, as well as requests
     for(i = 0; i < listFriends.length; i++){
         let friendsEmail = listFriends[i].friend_email;
@@ -41,13 +41,13 @@ router.post('/friendrequests', loggedin, function (req,res,next) {
     let friendemail = body.email.replace('/','');
     console.log(friendemail)
     if(status == 2){ //Accept case
-        connection.query("UPDATE friendstest SET status = 0 WHERE email = $1 AND friend_email = $2", [req.session.email, friendemail]);
-        connection.query("UPDATE friendstest SET status = 0 WHERE email = $1 AND friend_email = $2", [friendemail, req.session.email]);
+        connection.query("UPDATE friends SET status = 0 WHERE email = $1 AND friend_email = $2", [req.session.email, friendemail]);
+        connection.query("UPDATE friends SET status = 0 WHERE email = $1 AND friend_email = $2", [friendemail, req.session.email]);
     }
     //Either cancel, or delete friend/friend req. Does same thing to database
     else{
-        connection.query("DELETE FROM friendstest WHERE email = $1 AND friend_email = $2",[req.session.email, friendemail])
-        connection.query("DELETE FROM friendstest WHERE email = $1 AND friend_email = $2",[friendemail, req.session.email])
+        connection.query("DELETE FROM friends WHERE email = $1 AND friend_email = $2",[req.session.email, friendemail])
+        connection.query("DELETE FROM friends WHERE email = $1 AND friend_email = $2",[friendemail, req.session.email])
     }
     res.redirect('viewfriends');
 });
@@ -63,12 +63,12 @@ router.post('/addFriends',loggedin, function(req,res,next) {
     console.log("AddFriends");
     console.log(req.body);
 
-    connection.query("INSERT INTO friendstest (email, friend_email, status) VALUES ($1, $2, $3);",
+    connection.query("INSERT INTO friends (email, friend_email, status) VALUES ($1, $2, $3);",
         [req.session.email,friendEmail, 1],
         function(err,result){
         if(err) return next(err);
         });
-    connection.query("INSERT INTO friendstest (email, friend_email, status) VALUES ($1, $2, $3);",
+    connection.query("INSERT INTO friends (email, friend_email, status) VALUES ($1, $2, $3);",
         [friendEmail,req.session.email, 2],
         function(err,result){
             if(err) return next(err);
@@ -87,7 +87,7 @@ router.get('/searchFriends',loggedin, async function(req,res,next) {
     const searchFriends = '%' + req.query.search_friends.replace(/%/g, '') + '%';
 
     if (searchFriends) {
-        let result = await connection.query("SELECT * FROM personal_details WHERE (first_name ILIKE $1 OR last_name ILIKE $1) AND email NOT IN(SELECT friend_email FROM friendstest WHERE email = $2) AND email <> $2 ", [searchFriends, req.session.email]);
+        let result = await connection.query("SELECT * FROM personal_details WHERE (first_name ILIKE $1 OR last_name ILIKE $1) AND email NOT IN(SELECT friend_email FROM friends WHERE email = $2) AND email <> $2 ", [searchFriends, req.session.email]);
         for(i = 0; i< result.rows.length;i++){
             let email = result.rows[i].email;
             let name = result.rows[i].first_name + ' ' + result.rows[i].last_name;
